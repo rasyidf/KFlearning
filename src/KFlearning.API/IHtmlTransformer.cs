ad2585ed54e6401640c97d8e9bfb3eba914317f7
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2019-06-29 20:32:10 +0700

    Implement HTML img to base64 transformer

---
+++ src/KFlearning.API/IHtmlTransformer.cs
@@ -0,0 +1,13 @@
+﻿// // PROJECT :
+// // PROGRAM :
+// // AUTHOR  :   Fahmi Noor Fiqri
+// // NPM     :   065118116
+// // TANGGAL :   05 Mei 2019
+
+namespace KFlearning.API
+{
+    public interface IHtmlTransformer
+    {
+        string TransformHtml(string url);
+    }
+}
