55b725ea9c6267c67c98e3512c666c9bc92e4a96
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2019-07-07 09:43:14 +0700

    Add icon to project

--- src/KFlearning.Installer/Views/MainForm.Designer.cs
+++ src/KFlearning.Installer/Views/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace KFlearning.Installer
+﻿namespace KFlearning.Installer.Views
 {
     partial class MainForm
     {
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(549, 392);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KFlearning Installer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ src/KFlearning.Installer/Views/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace KFlearning.Installer.Views
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
