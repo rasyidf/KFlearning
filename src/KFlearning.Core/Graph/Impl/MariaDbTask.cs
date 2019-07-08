00fbfb7407f28ab9b94204df6a6f4919b28089d0
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2019-07-08 21:41:17 +0700

    Implemented task node

--- src/KFlearning.Core/Graph/Impl/MariaDbTask.cs
+++ src/KFlearning.Core/Graph/Impl/MariaDbTask.cs
@@ -1,23 +1,119 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading;
+using KFlearning.Core.IO;
 
-namespace KFlearning.Core.Graph.Impl
+namespace KFlearning.Core.Graph
 {
     public class MariaDbTask : ITaskNode
     {
-        public string TaskName => "MariaDB configuration";
-        public bool HasDependencies { get; }
-        public Queue<ITaskNode> Dependencies { get; }
+        #region Fields
+        
+        private IProgressBroker _broker;
+        private IPathManager _pathManager;
+        private InstallMode _mode;
+
+        #endregion
+
+        #region ITaskNode Properties
+        
+        public string TaskName => "MariaDB";
+        public bool HasDependencies => true;
+        public Queue<ITaskNode> Dependencies { get; private set; }
+
+        #endregion
+
+        #region ITaskNode Methods
+        
+        public void Configure(InstallerDefinition definition)
+        {
+            _pathManager = definition.ResolveService<IPathManager>();
+            _broker = definition.ResolveService<IProgressBroker>();
+            _mode = definition.Mode;
+
+            Dependencies = new Queue<ITaskNode>();
+            if (definition.Mode == InstallMode.Install)
+            {
+                var fileName = Path.GetFileName(definition.Packages.MariaDbUri.AbsoluteUri);
+                var savePath = _pathManager.GetPathForTemp(fileName);
+                
+                Dependencies.Enqueue(new DownloadTask(definition.Packages.MariaDbUri, savePath));
+                Dependencies.Enqueue(new ExtractTask(savePath, _pathManager.GetPath(PathKind.MariaDbInstallRoot)));
+            }
+            else
+            {
+                Dependencies.Enqueue(new DeleteFilesTask(_pathManager.GetPath(PathKind.MariaDbInstallRoot)));
+            }
+        }
 
         public bool Run(CancellationToken cancellation)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _broker.ReportProgress(0);
+                if (_mode == InstallMode.Install)
+                {
+                    _broker.ReportMessage("Installing MariaDB...");
+                    InternalInstall();
+                }
+                else
+                {
+                    _broker.ReportMessage("Uninstalling MariaDB...");
+                    InternalUninstall();
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _broker.ReportProgress(100);
+                _broker.ReportMessage(e.ToString());
+                return false;
+            }
+        } 
+
+        #endregion
+
+        #region Private Methods
+        
+        private void InternalInstall()
+        {
+            // find root directory
+            var rootDir = Directory.GetDirectories(_pathManager.GetPath(PathKind.MariaDbInstallRoot), "*",
+                SearchOption.TopDirectoryOnly).First();
+            var rootDirBackslash = _pathManager.EnsureBackslashEnding(rootDir);
+
+            // create config (my.ini)
+            _broker.ReportProgress(50);
+            _broker.ReportMessage("Configuring MariaDB...");
+
+            var template = new StringBuilder(Constants.MariaDbConfig);
+            template.Replace("{MARIADB_INSTALL_ROOT}", rootDirBackslash);
+
+            var myiniPath = Path.Combine(rootDir, "my.ini");
+            File.WriteAllText(myiniPath, template.ToString());
+
+            // add to env vars
+            _broker.ReportProgress(80);
+            _broker.ReportMessage("Adding MariaDB to environment variable...");
+
+            var path = Path.Combine(rootDir, "bin");
+            _pathManager.AddPathEnvironmentVar(path);
         }
 
-        public void Stop()
+        private void InternalUninstall()
         {
-            throw new NotImplementedException();
+            // remove from env vars
+            _broker.ReportProgress(70);
+            _broker.ReportMessage("Removing MariaDB from environment variable...");
+
+            _pathManager.RemovePathEnvironmentVar(_pathManager.GetPath(PathKind.MariaDbInstallRoot));
         }
+
+        #endregion
+        
     }
 }
--- src/KFlearning.Core/Graph/Impl/MingwTask.cs
+++ src/KFlearning.Core/Graph/Impl/MingwTask.cs
@@ -1,45 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using KFlearning.Core.IO;
 
-namespace KFlearning.Core.Graph.Impl
+namespace KFlearning.Core.Graph
 {
     public class MingwTask : ITaskNode
     {
-        private readonly IPathManager _pathManager;
+        #region Fields
+        
+        private IProgressBroker _broker;
+        private IProcessManager _processManager;
+        private IPathManager _pathManager;
         private InstallMode _mode;
 
-        public string TaskName => "MinGW configuration";
+        #endregion
+
+        #region ITaskNode Properties
+        
+        public string TaskName => "MinGW Compiler Suite";
         public bool HasDependencies => true;
         public Queue<ITaskNode> Dependencies { get; private set; }
+
+        #endregion
+
+        #region ITaskNode Methods
         
         public void Configure(InstallerDefinition definition)
         {
+            _pathManager = definition.ResolveService<IPathManager>();
+            _processManager = definition.ResolveService<IProcessManager>();
+            _broker = definition.ResolveService<IProgressBroker>();
             _mode = definition.Mode;
+
             Dependencies = new Queue<ITaskNode>();
-            if (_mode == InstallMode.Install)
+            if (definition.Mode == InstallMode.Install)
             {
-                
-                Dependencies.Enqueue(new DownloadTask(definition.Packages.MingwUri, ""));
-                Dependencies.Enqueue(new ExtractTask("",""));
+                var fileName = Path.GetFileName(definition.Packages.MingwUri.AbsoluteUri);
+                var savePath = _pathManager.GetPathForTemp(fileName);
+                Dependencies.Enqueue(new DownloadTask(definition.Packages.MingwUri, savePath));
+                Dependencies.Enqueue(new ExtractTask(savePath, _pathManager.GetPath(PathKind.MingwInstallRoot)));
+
+                fileName = Path.GetFileName(definition.Packages.GlutUri.AbsoluteUri);
+                savePath = _pathManager.GetPathForTemp(fileName);
+                Dependencies.Enqueue(new DownloadTask(definition.Packages.GlutUri, savePath));
+                Dependencies.Enqueue(new ExtractTask(savePath, _pathManager.GetPathForTemp("")));
             }
             else
             {
-                Dependencies.Enqueue(new DeleteFilesTask(""));
+                Dependencies.Enqueue(new DeleteFilesTask(_pathManager.GetPath(PathKind.MingwInstallRoot)));
             }
-        }
+        } 
 
         public bool Run(CancellationToken cancellation)
         {
             try
             {
+                _broker.ReportProgress(-1);
                 if (_mode == InstallMode.Install)
                 {
+                    _broker.ReportMessage("Installing MinGW...");
                     InternalInstall();
                 }
                 else
                 {
+                    _broker.ReportMessage("Uninstalling MinGW...");
                     InternalUninstall();
                 }
 
@@ -47,19 +73,51 @@ namespace KFlearning.Core.Graph.Impl
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                _broker.ReportProgress(100);
+                _broker.ReportMessage(e.ToString());
                 return false;
             }
         }
 
+        #endregion
+
+        #region Private Methods
+        
         private void InternalInstall()
         {
+            var root = _pathManager.GetPath(PathKind.MingwInstallRoot);
 
+            // install using mingw-get
+            var file = Path.Combine(root, @"bin\mingw-get.exe");
+            _processManager.RunWait(file, "install gcc g++ gdb mingw32-make");
+
+            // install glut library to mingw
+            var glutPath = Path.Combine(_pathManager.GetPathForTemp(""), "GLUTMingw32");
+            _pathManager.RecursiveMoveDirectory(Path.Combine(glutPath, "lib"), Path.Combine(root, "lib"));
+            _pathManager.RecursiveMoveDirectory(Path.Combine(glutPath, "include"), Path.Combine(root, "include"));
+            
+            // install glut to system
+            var glutDllSource = Path.Combine(Path.Combine(glutPath, "glut32.dll"));
+            var glutDllDest = Path.Combine(Environment.SystemDirectory, "glut32.dll");
+            File.Move(glutDllSource, glutDllDest);
+
+            // add to environment variable
+            _broker.ReportProgress(80);
+            _broker.ReportMessage("Adding MinGW Compiler Suite to environment variable...");
+
+            var path = Path.Combine(root, "bin");
+            _pathManager.AddPathEnvironmentVar(path);
         }
 
         private void InternalUninstall()
         {
-
+            // remove from environment variable
+            _broker.ReportProgress(70);
+            _broker.ReportMessage("Removing MinGW Compiler Suite from environment variable...");
+            
+            _pathManager.RemovePathEnvironmentVar(_pathManager.GetPath(PathKind.MingwInstallRoot));
         }
+        
+        #endregion
     }
 }
