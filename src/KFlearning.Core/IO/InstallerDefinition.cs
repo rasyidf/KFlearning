706a1bc663921b7a7e25c432a6e1c91a308dfaa9
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2019-07-08 21:42:51 +0700

    Add component resolver function

--- src/KFlearning.Core/IO/InstallerDefinition.cs
+++ src/KFlearning.Core/IO/InstallerDefinition.cs
@@ -1,15 +1,24 @@
-﻿using KFlearning.Core.API;
+﻿using System;
+using KFlearning.Core.API;
 
 namespace KFlearning.Core.IO
 {
     public class InstallerDefinition
     {
+        private readonly Func<Type, object> _resolveFunc;
+
+        public InstallerDefinition(Func<Type, object> resolveFunc)
+        {
+            _resolveFunc = resolveFunc;
+        }
+
         public InstallMode Mode { get; set; }
 
         public PackageConfig Packages { get; set; }
-
-        public string TempPath { get; set; }
-
         
+        public T ResolveService<T>()
+        {
+            return (T) _resolveFunc(typeof(T));
+        }
     }
 }
