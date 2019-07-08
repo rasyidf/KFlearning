4a4d7238c2dfa47e37a1391a423c2b68de863c99
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2019-07-08 21:35:14 +0700

    Add missing packages

--- src/KFlearning.Core/API/PackageConfig.cs
+++ src/KFlearning.Core/API/PackageConfig.cs
@@ -1,21 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KFlearning.Core.API
 {
     public class PackageConfig
     {
         public Uri MingwUri { get; set; }
+        public Uri GlutUri { get; set; }
 
         public Uri ApacheUri { get; set; }
 
         public Uri MariaDbUri { get; set; }
 
         public Uri PhpUri { get; set; }
-
         public Uri Xdebuguri { get; set; }
-
         public Uri PhpmyadminUri { get; set; }
 
-        public Uri KflearningUrl { get; set; }
+        public Uri VscodeUri { get; set; }
+        public List<Uri> VscodeExtensions { get; set; }
+        public List<Uri> ProjectTemplates { get; set; }
+
+        public Uri KflearningUri { get; set; }
     }
 }
