3875106fbb6cbd30757df6097c5c98e94704e6b7
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2019-07-08 21:42:15 +0700

    Implemented task graph runner

--- src/KFlearning.Core/Graph/TaskGraph.cs
+++ src/KFlearning.Core/Graph/TaskGraph.cs
@@ -4,47 +4,101 @@ using KFlearning.Core.IO;
 
 namespace KFlearning.Core.Graph
 {
-    public class TaskGraph
+    public class TaskGraph : ITaskGraph
     {
-        public void RunGraph()
+        #region Fields
+        
+        private readonly IProgressBroker _progressBroker;
+
+        private CancellationTokenSource _tokenSource;
+        private Thread _thread;
+        private InstallerDefinition _definition;
+        private ITaskNode _rootNode;
+
+        #endregion
+
+        #region Properties
+        
+        public bool IsRunning => _tokenSource != null;
+
+        #endregion
+
+        #region Constructor
+        
+        public TaskGraph(IProgressBroker progressBroker)
         {
-            
+            _progressBroker = progressBroker;
         }
 
-        private void InternalRunGraph(InstallerDefinition definition, ITaskNode rootNode, CancellationToken token)
+        #endregion
+
+        #region Public Methods
+
+        public void RunGraph(InstallerDefinition definition, ITaskNode rootNode)
         {
+            Cancel();
+            _definition = definition;
+            _rootNode = rootNode;
+
+            _tokenSource = new CancellationTokenSource();
+            _thread = new Thread(ThreadCallback);
+            _thread.Start();
+        }
+
+        public void Cancel()
+        {
+            if (_tokenSource == null) return;
+            _tokenSource.Dispose();
+            _tokenSource = null;
+
+            _thread.Join();
+            _thread = null;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ThreadCallback()
+        {
+            try
+            {
+                InternalRunGraph(_definition, _rootNode);
+            }
+            catch (OperationCanceledException)
+            {
+                _progressBroker.ReportMessage("OPERATION CANCELED.");
+                _progressBroker.ReportProgress(100);
+            }
+            catch (Exception ex)
+            {
+                _progressBroker.ReportMessage("OPERATION FAULTED.\r\n" + ex);
+                _progressBroker.ReportProgress(100);
+            }
+        }
+
+        private void InternalRunGraph(InstallerDefinition definition, ITaskNode rootNode)
+        {
+            _progressBroker.ReportMessage("Running node: " + rootNode.TaskName);
             rootNode.Configure(definition);
             if (rootNode.HasDependencies)
             {
+                _progressBroker.ReportMessage($"Node has {rootNode.Dependencies.Count} dependencies, running dependencies...");
                 foreach (ITaskNode nodeDependency in rootNode.Dependencies)
                 {
-                    InternalRunGraph(definition, nodeDependency, token);
+                    _progressBroker.ReportMessage("Running dependency: " + rootNode.TaskName);
+                    InternalRunGraph(definition, nodeDependency);
                 }
             }
-            
-            rootNode.Run(token);
-            if (rootNode is IDisposable disposable) disposable.Dispose();
-        }
 
-        
+            rootNode.Run(_tokenSource.Token);
+            _progressBroker.ReportMessage("Node finished: " + rootNode.TaskName);
 
-        private void BuildInstallGraph()
-        {
-            // step 1 - kill all process
+            if (!(rootNode is IDisposable disposable)) return;
+            disposable.Dispose();
+            _progressBroker.ReportMessage("Node disposed: " + rootNode.TaskName);
+        } 
 
-            // step 2 - mingw
-
-            // step 2 - mariadb
-
-            // step 3 - apache httpd
-
-            // step 4a - php
-            
-            // step 4b - xdebug
-            
-            // step 5a - phpmyadmin
-
-            // step 6 - kflearning
-        }
+        #endregion
     }
 }
