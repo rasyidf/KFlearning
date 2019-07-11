95adf4f8c7d09cf1806f410c1b89223275026b62
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2019-07-11 20:23:31 +0700

    Add kflearning task

--- src/KFlearning.Core/Graph/InstallGraph.cs
+++ src/KFlearning.Core/Graph/InstallGraph.cs
@@ -6,29 +6,33 @@ namespace KFlearning.Core.Graph
 {
     public class InstallGraph : ITaskNode
     {
+        #region ITaskNode Properties
+        
         public string TaskName => "Install graph root";
         public bool HasDependencies => true;
-        public Queue<ITaskNode> Dependencies { get; }
+        public Queue<ITaskNode> Dependencies { get; private set; }
 
-        public InstallGraph(MingwTask mingw, MariaDbTask mariaDb, VscodeTask vscode, InitializeDirectoriesTask initializeDirectories)
-        {
-            Dependencies = new Queue<ITaskNode>();
-            Dependencies.Enqueue(initializeDirectories);
-            Dependencies.Enqueue(mingw);
-            // apache
-            // php
-            Dependencies.Enqueue(mariaDb);
-            Dependencies.Enqueue(vscode);
-            // kflearning
-        }
-        
+        #endregion
+
+        #region ITaskNode Methods
+
         public void Configure(InstallerDefinition definition)
         {
+            Dependencies = new Queue<ITaskNode>();
+            Dependencies.Enqueue(definition.ResolveService<InitializeDirectoriesTask>());
+            Dependencies.Enqueue(definition.ResolveService<MingwTask>());
+            //Dependencies.Enqueue(definition.ResolveService<InitializeDirectoriesTask>()); - apache
+            //Dependencies.Enqueue(definition.ResolveService<InitializeDirectoriesTask>()); - php
+            Dependencies.Enqueue(definition.ResolveService<MariaDbTask>());
+            Dependencies.Enqueue(definition.ResolveService<VscodeTask>());
+            Dependencies.Enqueue(definition.ResolveService<KflearningTask>());
         }
 
         public bool Run(CancellationToken cancellation)
         {
             return true;
-        }
+        } 
+
+        #endregion
     }
 }
