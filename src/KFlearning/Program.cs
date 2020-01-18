e1d8608a4482aef7ae38931cd0a2c4574f83d52e
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2020-01-18 12:17:13 +0700

    Remove startup checking

--- src/KFlearning/Program.cs
+++ src/KFlearning/Program.cs
@@ -8,7 +8,6 @@ namespace KFlearning
     static class Program
     {
         public static WindsorContainer Container = new WindsorContainer();
-        public static bool InstallMode;
 
         /// <summary>
         /// The main entry point for the application.
@@ -20,21 +19,7 @@ namespace KFlearning
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            if (args.Length > 0 && args[0] == "--install")
-            {
-                InstallMode = true;
-                Application.Run(Container.Resolve<InstallerForm>());
-            }
-            else if (args.Length > 0 && args[0] == "--uninstall")
-            {
-                InstallMode = false;
-                Application.Run(Container.Resolve<InstallerForm>());
-            }
-            else
-            {
-                Application.Run(Container.Resolve<StartupForm>());
-            }
+            Application.Run(Container.Resolve<StartupForm>());
         }
     }
 }
